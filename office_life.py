fd4d0845036eedecb0cb56b9fb6c8f0e2e4d1ac3
jarek <jaroslaw.sokolik@clicktrans.pl>
2021-10-18 12:29:25 +0200

    OOP employee project practice

---
+++ office_life.py
@@ -0,0 +1,13 @@
+from Developers.developers import Developer
+from Management.management import Management
+from Office.office import Office
+
+boss = Management("Bossy", "Kowalski", 2300, "Angry")
+boss.full_name()
+boss.count_company_income()
+developer_1 = Developer("John", "Smith", 2300, "Java", "Intermediate")
+developer_1.full_name()
+developer_1.print_skill_level()
+office_worker_1 = Office("Anna", "Smith", 500, "Iphone 10")
+office_worker_1.full_name()
+office_worker_1.print_provided_phone()
