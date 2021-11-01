6def365b75bd1aeae83741395ec1c27dc5635292
Jarosław Sokolik <jaroslaw.sokolik@clicktrans.pl>
2021-11-01 11:42:44 +0100

    nauka

--- Office/office.py
+++ Office/office.py
@@ -5,7 +5,11 @@ class Office(Employee):
     def __init__(self, first_name, last_name, pay, phone):  # Office init called
         super().__init__(first_name, last_name, pay)  # Employee init called
         self.phone = phone
+        self.time_to_respond = 30
 
     def print_provided_phone(self):
         print("I am eqquped with " + self.phone)
+
+    def respond_time(self):
+        print("My time to respond is " + str(self.time_to_respond) + " seconds")
 
